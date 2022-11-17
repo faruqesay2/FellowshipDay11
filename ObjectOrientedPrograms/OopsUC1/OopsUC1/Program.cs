2e82ccab43fa9cc01ad3c7a117e2da13304143cf
faruqesay2 <faruqesay786@gmail.com>
2022-11-17 19:27:21 +0530

    Stock_Inventory_Management

---
+++ ObjectOrientedPrograms/OopsUC1/OopsUC1/Program.cs
@@ -0,0 +1,69 @@
+﻿using ObjectOrientedPrograms.Inventory_ManagementSystem;
+using ObjectOrientedPrograms.Inventorymanagement;
+using ObjectOrientedPrograms.StockManagement;
+using ObjectOrientedPrograms.StockManagementSystem;
+using System;
+using Stock = ObjectOrientedPrograms.StockManagementSystem.Stock;
+
+namespace OopsUC1
+{
+    public class Program
+    {
+        const string INVENTORY_DATA_FILE_PATH = @"C:\Users\Lenovo\Desktop\Git\JsonFile\ObjectOrientedPrograms\Inventorymanagement\Inventory.json";
+        const string INVENTORYDETAILS_DATA_FILE_PATH = @"C:\Users\Lenovo\Desktop\Git\JsonFile\ObjectOrientedPrograms\Inventory ManagementSystem\InventoryDetails.json";
+        const string STOCKDETAILS_DATA_FILE_PATH = @"C:\Users\Lenovo\Desktop\Git\JsonFile\ObjectOrientedPrograms\StockManagement\StockDetails.json";
+        const string STOCK_DATA_FILE_PATH = @"C:\Users\Lenovo\Desktop\Git\JsonFile\ObjectOrientedPrograms\StockManagementSystem\Stock.json";
+        const string COMPANY_DATA_FILE_PATH = @"C:\Users\Lenovo\Desktop\Git\JsonFile\ObjectOrientedPrograms\StockManagementSystem\Company.json";
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Select Programs\n 1.Inventory Management \n2.Inventory Manangement System \n3.Stock Management\n4.Stock Management System ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Inventory inventory = new Inventory();
+                        inventory.ReadJsonFile(INVENTORY_DATA_FILE_PATH);
+                        break;
+                    case 2:
+                        InventoryFactory inventoryFactory = new InventoryFactory();
+                        inventoryFactory.ReadJsonFile(INVENTORYDETAILS_DATA_FILE_PATH);
+                        InventoryDetails details = new InventoryDetails()
+
+                        {
+                            Name = "a",
+                            Weight = 5,
+                            Price = 25
+                        };
+                        inventoryFactory.AddInventory("WheatList", details);
+                        //inventoryFactory.DeleteInventory("WheatList", "a");
+                        inventoryFactory.WriteTojson(INVENTORYDETAILS_DATA_FILE_PATH);
+                        break;
+                    case 3:
+                    //Stock stock = new Stock();
+                    //stock.ReadJsonFile(STOCKDETAILS_DATA_FILE_PATH);
+                    //break;
+                    case 4:
+                        Stock1Management stockManagement = new Stock1Management();
+                        stockManagement.ReadJsonFileStock(STOCK_DATA_FILE_PATH);
+                        stockManagement.ReadJsonFileCompany(COMPANY_DATA_FILE_PATH);
+                        //Company company = new Company()
+                        Stock stock = new Stock()
+                        {
+                            Name = "Facebook",
+                            NoOfShares = 5,
+                            PricePerShare = 250,
+                        };
+                        stockManagement.SellStockShares(stock);
+                        //stockManagement.BuyCompanyShares(company);
+                        stockManagement.WriteToJsonStock(STOCK_DATA_FILE_PATH);
+                        stockManagement.WriteToJsoncompany(COMPANY_DATA_FILE_PATH);
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ ObjectOrientedPrograms/OopsUC3/OopsUC3/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace OopsUC3
+{
+    public class Stock
+    {
+        List<StockDetails> details = new List<StockDetails>();
+
+        public void ReadJsonFile(string fileName)
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                var json = reader.ReadToEnd();
+                this.details = JsonConvert.DeserializeObject<List<StockDetails>>(json);
+                Console.WriteLine("Name" + "\t" + "Share" + "\t" + "SharePrice" + "\t" + "TotalSharePrice");
+                foreach (var data in details)
+                {
+                    Console.WriteLine(data.Name + "\t" + data.Share + "\t" + data.SharePrice + "\t" + data.Share * data.SharePrice);
+                }
+            }
+        }
+    }
+}
